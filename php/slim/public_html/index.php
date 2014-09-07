2829639466975f11dba2d64573ebcb4e15a2220d
ebargtuo <ebargtuo@gmx.it>
2014-09-07 08:48:23 +0000

    Set response content type to application/json in slim framework example

--- php/slim/public_html/index.php
+++ php/slim/public_html/index.php
@@ -5,10 +5,12 @@ $app = new \Slim\Slim();
 
 
 $app->get('/hello', function() use ($app) {
+    $app->response->headers->set('Content-Type', 'application/json');
     $app->response->write(json_encode(array("message" => "Hello")));
 });
 
 $app->get('/hello/:name', function($name) use ($app) {
+    $app->response->headers->set('Content-Type', 'application/json');
     $app->response->write(json_encode(array(
         "message" => "Hello " . ucfirst($name)
     )));
